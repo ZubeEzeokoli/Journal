bb0ebeacb9d901d4e04b52f4c84a94c83fc1ac42
Nzubechi Ezeokoli <62896253+ZubeEzeokoli@users.noreply.github.com>
2022-02-04 12:51:47 -0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,12 +1,3 @@
-# input_processor.py
-
-# Starter code for assignment 2 in ICS 32 Programming with Software Libraries in Python
-
-# Replace the following placeholders with your information.
-
-# Nzubechi Ezeokoli
-# nezeokol@uci.edu
-# 56611321
 from pathlib import Path
 from Profile import Profile
 from Profile import Post
